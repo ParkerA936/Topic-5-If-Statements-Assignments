9ee7a9a7c251ee6fb59b6d1014f938ec3bf91ee5
Parker Arnett <362581936@GoTVDSB.ca>
2026-02-23 12:03:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Topic_5___If_Statements_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            //Parker
+        }
+        public static void Part1()
+        {
+            //Space Boxing
+            int weight;
+            Console.WriteLine("How much do you weigh on earth?");
+            int.TryParse(Console.ReadLine(), out weight);
+            Console.WriteLine("I have info on the folowing planets.");
+            Console.WriteLine("1.Venus   2.Mars    3.Jupiter");
+            Console.WriteLine("4.Saturn  5.Uranus  6.Neptune");
+            Console.WriteLine("What planet are you visiting?");
+
+
+        }
+    }
+}
